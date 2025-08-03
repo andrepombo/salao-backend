78dd150a52ed19dd91d7b4aa60454edd03ce25c6
lock-desk <pintepinturasdeveloper@gmail>
2025-08-03 13:58:12 -0300

    refactor: update phone number format to (xxx) xxxxx-xxxx pattern for clients and team models

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -13,10 +13,10 @@ class Client(models.Model):
     
     name = models.CharField(max_length=100)
     phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+        regex=r'^\(\d{3}\) \d{5}-\d{4}$',
+        message="Phone number must be entered in the format: '(xxx) xxxxx-xxxx'."
     )
-    phone = models.CharField(validators=[phone_regex], max_length=17)
+    phone = models.CharField(validators=[phone_regex], max_length=16)
     email = models.EmailField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     birthday = models.DateField(blank=True, null=True, help_text="Client's date of birth")
--- apps/team/models.py
+++ apps/team/models.py
@@ -7,10 +7,10 @@ class Team(models.Model):
     """Model for salon team members (hairdressers)"""
     name = models.CharField(max_length=100)
     phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+        regex=r'^\(\d{3}\) \d{5}-\d{4}$',
+        message="Phone number must be entered in the format: '(xxx) xxxxx-xxxx'."
     )
-    phone = models.CharField(validators=[phone_regex], max_length=17)
+    phone = models.CharField(validators=[phone_regex], max_length=16)
     email = models.EmailField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     specialties = models.ManyToManyField(
